6587dfba94b0cdb748b976966eee5f2c4df9ddd0
laynia <vc_mikuu@hotmail.com>
2024-08-12 15:01:34 +0200

    Fix for 1.37 #1

--- include/main.hpp
+++ include/main.hpp
@@ -24,8 +24,16 @@
 
 void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
 
-// Define these functions here so that we can easily read configuration and
-// log information from other files
-Configuration &getConfig();
+/// @brief Stores the ID and version of our mod, and is sent to the modloader upon startup
+modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 
-constexpr auto PaperLogger = Paper::ConstLoggerContext("darthmaul");
+/// @brief A logger, useful for printing debug messages
+/// @return
+static constexpr auto Logger = Paper::ConstLoggerContext(MOD_ID "_" VERSION);
+
+#define MOD_EXPORT __attribute__((visibility("default")))
+#ifdef __cplusplus
+#define MOD_EXPORT_FUNC extern "C" MOD_EXPORT
+#else
+#define MOD_EXPORT_FUNC MOD_EXPORT
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -64,51 +64,51 @@ MAKE_HOOK_MATCH(PlayerTransforms_Update, &PlayerTransforms::Update, void, Player
     if(getMainConfig().Active.GetValue()){
         if(getMainConfig().OneHand.GetValue()){
             if(getMainConfig().LeftHanded.GetValue()){
-                self->rightHandTransform->get_transform()->set_localPosition({
-                    self->leftHandTransform->get_position().x,
-                    self->leftHandTransform->get_position().y,
-                    self->leftHandTransform->get_position().z
+                self->_rightHandTransform->get_transform()->set_localPosition({
+                    self->_leftHandTransform->get_position().x,
+                    self->_leftHandTransform->get_position().y,
+                    self->_leftHandTransform->get_position().z
                 });
 
-                self->rightHandTransform->get_transform()->set_eulerAngles({
-                    self->leftHandTransform->get_eulerAngles().x + 180,
-                    self->leftHandTransform->get_eulerAngles().y,
-                    self->leftHandTransform->get_eulerAngles().z,
+                self->_rightHandTransform->get_transform()->set_eulerAngles({
+                    self->_leftHandTransform->get_eulerAngles().x + 180,
+                    self->_leftHandTransform->get_eulerAngles().y,
+                    self->_leftHandTransform->get_eulerAngles().z,
                 });
             } else{
                 self->leftHandTransform->get_transform()->set_localPosition({
-                    self->rightHandTransform->get_position().x,
-                    self->rightHandTransform->get_position().y,
-                    self->rightHandTransform->get_position().z
+                    self->_rightHandTransform->get_position().x,
+                    self->_rightHandTransform->get_position().y,
+                    self->_rightHandTransform->get_position().z
                 });
 
                 self->leftHandTransform->get_transform()->set_eulerAngles({
-                    self->rightHandTransform->get_eulerAngles().x + 180,
-                    self->rightHandTransform->get_eulerAngles().y,
-                    self->rightHandTransform->get_eulerAngles().z,
+                    self->_rightHandTransform->get_eulerAngles().x + 180,
+                    self->_rightHandTransform->get_eulerAngles().y,
+                    self->_rightHandTransform->get_eulerAngles().z,
                 });
             }
             
         } else{
-            xa = self->rightHandTransform->get_position().x;
-            ya = self->rightHandTransform->get_position().y;
-            za = self->rightHandTransform->get_position().z;
+            xa = self->_rightHandTransform->get_position().x;
+            ya = self->_rightHandTransform->get_position().y;
+            za = self->_rightHandTransform->get_position().z;
 
-            xb = self->leftHandTransform->get_position().x;
-            yb = self->leftHandTransform->get_position().y;
-            zb = self->leftHandTransform->get_position().z;
+            xb = self->_leftHandTransform->get_position().x;
+            yb = self->_leftHandTransform->get_position().y;
+            zb = self->_leftHandTransform->get_position().z;
 
             float xAverage = (xa + xb) / 2;
             float yAverage = (ya + yb) / 2;
             float zAverage = (za + zb) / 2;
 
-            Quaternion rotation = UnityEngine::Quaternion::LookRotation(self->rightHandTransform->get_position() - self->leftHandTransform->get_position());
+            Quaternion rotation = UnityEngine::Quaternion::LookRotation(self->_rightHandTransform->get_position() - self->_leftHandTransform->get_position());
         
-            self->rightHandTransform->get_transform()->set_localPosition({xAverage, yAverage, zAverage});
-            self->rightHandTransform->get_transform()->set_eulerAngles(rotation.get_eulerAngles());
+            self->_rightHandTransform->get_transform()->set_localPosition({xAverage, yAverage, zAverage});
+            self->_rightHandTransform->get_transform()->set_eulerAngles(rotation.get_eulerAngles());
             
-            self->leftHandTransform->get_transform()->set_localPosition({xAverage, yAverage, zAverage});
-            self->leftHandTransform->get_transform()->set_eulerAngles({
+            self->_leftHandTransform->get_transform()->set_localPosition({xAverage, yAverage, zAverage});
+            self->_leftHandTransform->get_transform()->set_eulerAngles({
                 rotation.get_eulerAngles().x + 180,
                 rotation.get_eulerAngles().y,
                 rotation.get_eulerAngles().z * -1,
@@ -136,28 +136,31 @@ void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedToH
 }
 
 
-// Called at the early stages of game loading
-MOD_EXTERN_FUNC void setup(CModInfo *info) noexcept {
-    info.id = ID;
+
+#pragma region Mod setup
+/// @brief Called at the early stages of game loading
+/// @param info
+/// @return
+MOD_EXPORT_FUNC void setup(CModInfo& info) {
+    info.id = MOD_ID;
     info.version = VERSION;
-    modInfo = info;
-	
-    getConfig().Load(); // Load the config file
-    getLogger().info("Completed setup!");
+
+    Logger.info("Completed setup!");
 }
 
-
-extern "C" void load() {
+/// @brief Called later on in the game loading - a good time to install function hooks
+/// @return
+MOD_EXPORT_FUNC void late_load() {
     il2cpp_functions::Init();
-    getMainConfig().Init(modInfo);
-
-    LoggerContextObject logger = getLogger().WithContext("load");
-    BSML::Init();
+      BSML::Init();
 
     BSML::Register::RegisterMainMenu("Darth Maul", "Darth Maul Settings", "Configure some darth maul settings", DidActivate);
 
-    getLogger().info("Installing hooks...");
-    INSTALL_HOOK(logger, PlayerTransforms_Update);
-    INSTALL_HOOK(logger, SaberClashEffect_LateUpdate);
-    getLogger().info("Installed all hooks!");
+    Logger.info("Installing hooks...");
+
+    INSTALL_HOOK(Logger, PlayerTransforms_Update);
+    INSTALL_HOOK(Logger, SaberClashEffect_LateUpdate);
+
+    Logger.info("Installed all hooks!");
 }
+#pragma endregion
